6359a3c89a14bea02f5d0f405152f95d5c95df50
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-10-30 16:49:22 +0900

    wasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move() {
+
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
     }
 }
